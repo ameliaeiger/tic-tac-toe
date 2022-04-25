bf150d1a147de49c0d2eaff4004abed8eefa84e7
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-04-24 23:43:23 -0600

    Commit

---
+++ file.js
@@ -0,0 +1,17 @@
+var winConditions = [
+  {square: 1, checkedBy: "" }
+]
+
+
+//When player makes choice of square, ID info grabbed from class
+var winConditions = {
+  squareOne: "",
+  squareTwo: "",
+  squareThree: "",
+  squareFour: "",
+  squareFive: "",
+  squareSix: "", 
+  squareSeven: "",
+  squareEight: "",
+  squareNine: "",
+}
